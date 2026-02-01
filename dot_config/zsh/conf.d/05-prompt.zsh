1fc2c36654221088a5b35da029b1086f912d22d1
mitsubosh <yuya.mitsuboshi@gmail.com>
2026-02-01 10:39:28 +0900

    Fix ANSI escape code

--- dot_config/zsh/conf.d/05-prompt.zsh
+++ dot_config/zsh/conf.d/05-prompt.zsh
@@ -1,4 +1,5 @@
 # Prompt configuration
+# From dot.zsh/default.sh
 
 autoload colors
 colors
@@ -7,18 +8,18 @@ setopt prompt_subst
 case ${UID} in
 0)
   [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
-  PROMPT="%B%{[36m%}[%n@%m]#%{[m%}%b "
-  PROMPT2="%B%{[31m%}%_#%{[m%}%b "
-  RPROMPT="%{[36m%}(`rprompt_git_current_branch`%{[36m%}%~%)%{[m%}"
-  SPROMPT="%B%{[31m%}%r is correct? [n,y,a,e]:%{${reset_color}%}%b "
+  PROMPT=$'%B%{\e[36m%}[%n@%m]#%{\e[m%}%b '
+  PROMPT2=$'%B%{\e[31m%}%_#%{\e[m%}%b '
+  RPROMPT=$'%{\e[36m%}(`rprompt_git_current_branch`%{\e[36m%}%~%)%{\e[m%}'
+  SPROMPT=$'%B%{\e[31m%}%r is correct? [n,y,a,e]:%{${reset_color}%}%b '
   [ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
   PROMPT="%{${fg[white]}%}${HOST%%.*} ${PROMPT}"
   ;;
 *)
-  PROMPT="%{[36m%}[%n@%m]%%%{[m%} "
-  PROMPT2="%{[31m%}%_%%%{[m%} "
-  RPROMPT='%{[36m%}(`rprompt_git_current_branch`%{[36m%}%~%)%{[m%}'
-  SPROMPT="%{[31m%}%r is correct? [n,y,a,e]:%{${reset_color}%} "
+  PROMPT=$'%{\e[36m%}[%n@%m]%%%{\e[m%} '
+  PROMPT2=$'%{\e[31m%}%_%%%{\e[m%} '
+  RPROMPT=$'%{\e[36m%}(`rprompt_git_current_branch`%{\e[36m%}%~%)%{\e[m%}'
+  SPROMPT=$'%{\e[31m%}%r is correct? [n,y,a,e]:%{${reset_color}%} '
   [ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
   PROMPT="%{${fg[white]}%}${HOST%%.*} ${PROMPT}"
   ;;
